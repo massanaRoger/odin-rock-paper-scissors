c53ce0a62d49a1ae67ce443a9614d8c49ad38c4b
Roger Massana <rogermassana2002@gmail.com>
2022-07-24 15:41:06 +0200

    Add function to get computer choice in script file

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(getComputerChoice())
+
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * 3);
+    return choices[random];
+}
