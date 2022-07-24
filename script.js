574025228f52cc845b355fe4dc273a698f502446
Roger Massana <rogermassana2002@gmail.com>
2022-07-24 15:52:37 +0200

    Add function to play a round

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-console.log(getComputerChoice())
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
+console.log(playRound("Rock", computerChoice));
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * 3);
     return choices[random];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerWin = false;
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        playerWin = true;
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    return playerWin ? `You won! ${playerSelection} wins ${computerSelection}` :
+        `You lose! ${computerSelection} wins ${playerSelection}`;
 }
