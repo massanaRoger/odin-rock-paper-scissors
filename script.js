f93fbc97d8f2b9780f992d223fcb239a74968f33
Roger Massana <rogermassana2002@gmail.com>
2022-07-25 22:16:11 +0200

    Play round 5 times and display overall winner

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-const buttons = document.querySelectorAll('.play-buttons');
-buttons.forEach(button => {
-    button.addEventListener('click', checkPlayerWins);
-})
+const resultsDiv = document.querySelector('.results-container');
+game();
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * 3);
     return choices[random];
-}
-
-function checkPlayerWins() {
-    const playerSelection = this.textContent;
-    const computerSelection = getComputerChoice();
-    const resultsDiv = document.querySelector('.results-container');
-    let playerWin = -1;
-    if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Paper" && computerSelection === "Rock") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        playerWin = 1;
-        addPToDOM(resultsDiv, `You won! ${playerSelection} wins ${computerSelection}`)
-    } else if (playerSelection === computerSelection) {
-        playerWin = 0;
-        addPToDOM(resultsDiv, "It's a tie!");
-    }
-    playerWin !== -1 || addPToDOM(resultsDiv, `You lose! ${computerSelection} wins ${playerSelection}`);
-    return playerWin;
 }
 
 function addPToDOM(elementToBeAdded, message) {
@@ -34,4 +14,41 @@ function addPToDOM(elementToBeAdded, message) {
     elementToBeAdded.appendChild(p);
 }
 
+function game() {
+    let playerPoints = 0, computerPoints = 0, i = 0;
+    const buttons = document.querySelectorAll('.play-buttons');
+    buttons.forEach(button => {
+        button.addEventListener('click', checkPlayerWins);
+    });
 
+
+    function checkPlayerWins() {
+        const playerSelection = this.textContent;
+        const computerSelection = getComputerChoice();
+        i++;
+        if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+            (playerSelection === "Paper" && computerSelection === "Rock") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper")) {
+            addPToDOM(resultsDiv, `You won! ${playerSelection} wins ${computerSelection}`);
+            playerPoints++;
+        } else if (playerSelection === computerSelection) {
+            addPToDOM(resultsDiv, "It's a tie!");
+        } else {
+            addPToDOM(resultsDiv, `You lose! ${computerSelection} wins ${playerSelection}`);
+            computerPoints++;
+        }
+        if (i >= 5)
+            displayEndWinner(playerPoints, computerPoints);
+
+    }
+}
+
+function displayEndWinner(playerPoints, computerPoints) {
+    if (playerPoints > computerPoints) {
+        addPToDOM(resultsDiv, `You won! Score: ${playerPoints} vs ${computerPoints}`)
+    } else if (playerPoints < computerPoints) {
+        addPToDOM(resultsDiv, `You lose! Score: ${playerPoints} vs ${computerPoints}`)
+    } else {
+        addPToDOM(resultsDiv, `It's a tie! Score: ${playerPoints} vs ${computerPoints}`)
+    }
+}
